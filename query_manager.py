110d900c6f43c57044cc37d0a2a504100744833e
Rafael Matito <rafa.matito@gmail.com>
2016-06-11 16:48:38 +0200

    Query manager enhancement

--- query_manager.py
+++ query_manager.py
@@ -10,10 +10,11 @@ class Manager():
         result = {}
 
         for hub in self.hubs:
-            today_morning = time.strftime("%Y-%m-%d") + ' 00:00:00'
-            today_night = time.strftime("%Y-%m-%d") + ' 23:59:59'
+            time_start = time.strftime("%Y-%m-%d") + ' 00:00:00'
+            time_end = time.strftime("%Y-%m-%d") + ' 23:59:59'
             sql = """
-                SELECT COUNT(*) AS num, sum(total_paid_real) as money
+                SELECT po.clothes_subtype AS subtype, 
+                COUNT(*) AS num, sum(total_paid_real) as money
                 FROM ps_orders o
                 INNER JOIN PercentilOrder po
                 ON o.id_order = po.id_order
@@ -22,16 +23,68 @@ class Manager():
                 AND po.id_order_state IN ({})
                 GROUP BY po.clothes_subtype
                 ORDER BY clothes_subtype ASC
-                """.format(today_morning, today_night, config.valid_order_states)
+                """.format(time_start, time_end, config.valid_order_states)
         
             data = hub['db'].run_query(sql)
             if data == None:
                 subtotal = {'num': '-', 'money': '-'}
             else:
-                num = data[0]['num'] + data[1]['num'] + data[2]['num']
-                money = data[0]['money'] + data[1]['money'] + data[2]['money']
+                num = money = 0
+                for value in data:
+                    num += value['num']
+                    money += value['money']
+
                 subtotal = {'num': num, 'money': money}
 
             result[hub['name']] = {'data': data, 'subtotal': subtotal}
 
         return result
+
+    def get_purchases_data(self):
+        result = {}
+
+        for hub in self.hubs:
+            #time_start = time.strftime("%Y-%m-%d") + ' 00:00:00'
+            #time_end = time.strftime("%Y-%m-%d") + ' 23:59:59'
+            time_start = '2016-06-03 00:00:00'
+            time_end = '2016-06-03 23:59:59'
+            sql = """
+                SELECT IF(pp.id_subproductType IN (1, 2, 3), 'K', 'W') AS subtype, COUNT(*) AS num, SUM(pp.fiscal_price) AS money
+                FROM PercentilProduct pp
+                WHERE pp.processedOn >= '{}'
+                AND pp.processedOn <= '{}'
+                AND pp.status NOT IN ('Lost', 'Discarded', 'Discarded_Hiper')
+                GROUP BY subtype
+                """.format(time_start, time_end)
+        
+            data = hub['db'].run_query(sql)
+            if data == None:
+                subtotal = {'num': '-', 'money': '-'}
+            else:
+                num = money = 0
+                for value in data:
+                    num += value['num']
+                    money += value['money']
+
+                subtotal = {'num': num, 'money': money}
+
+            result[hub['name']] = {'data': data, 'subtotal': subtotal}
+
+        return result
+
+    def get_num_wrong_orders(db):
+        today = time.strftime("%Y-%m-%d") + ' 00:00:00'
+        try:
+            db.query("""
+                SELECT COUNT(*) AS num
+                FROM ps_orders o
+                LEFT JOIN PercentilOrder po
+                ON o.id_order = po.id_order
+                WHERE o.date_add >= '{}'
+                AND (po.id_order IS NULL OR po.id_order_state IN ({}))
+                """.format(today, config.wrong_order_states))
+            r = db.store_result()
+            data = r.fetch_row()
+            return data[0][0]
+        except:
+            return '-'
--- telegram.py
+++ telegram.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from daemonize import Daemonize
+import emoji
 import config
 import custombot
 import db
@@ -8,19 +9,36 @@ import query_manager
 
 pid = "/tmp/percentil_telegram.pid"
 
+hubs = [
+    {
+        'name': 'madrid',
+        'db': db.Db(host=config.mad_db_host, user=config.mad_db_user, passwd=config.mad_db_pass, dbname=config.mad_db_name)
+    },
+    {
+        'name': 'berlin',
+        'db': db.Db(host=config.ber_db_host, user=config.ber_db_user, passwd=config.ber_db_pass, dbname=config.ber_db_name)
+    }
+]
+
+default_grouped_data = [
+    {
+        'name': 'Num',
+        'key': 'num',
+        'postfix': None,
+        'format': '{:1.0f}'
+    },
+    {
+        'name': 'Money',
+        'key': 'money',
+        'postfix': u"\u20AC",
+        'format': '{:1.2f}'
+    }
+]
+
 def main():
     bot = custombot.CustomBot(config.token)
 
-    hubs = [
-        {
-            'name': 'madrid',
-            'db': db.Db(host=config.mad_db_host, user=config.mad_db_user, passwd=config.mad_db_pass, dbname=config.mad_db_name)
-        },
-        {
-            'name': 'berlin',
-            'db': db.Db(host=config.ber_db_host, user=config.ber_db_user, passwd=config.ber_db_pass, dbname=config.ber_db_name)
-        }
-    ]
+
 
     manager = query_manager.Manager(hubs)
 
@@ -34,123 +52,25 @@ def main():
         	print_help(bot, message)
 
     ## /orders
-    @bot.message_handler(commands=['orders'])
+    @bot.message_handler(commands=['orders', 'o'])
     def response_orders(message):
         if not check_auth(message):
             response_no_access(bot, message);
             return
         else:
             data = manager.get_orders_data()
-            message_text = "<b>MADRID:</b>\n<b>Num:</b> K: <code>{}</code> | W: <code>{}</code> | M: <code>{}</code> | T: <code>{}</code>\n" \
-                "<b>Money:</b> K: <code>{}</code> | W: <code>{}</code> | M: <code>{}</code> | T: <code>{}</code>\n\n" \
-                "<b>BERLIN:</b>\n<b>Num:</b> K: <code>{}</code> | W: <code>{}</code> | M: <code>{}</code> | T: <code>{}</code>\n" \
-                "<b>Money:</b> K: <code>{}</code> | W: <code>{}</code> | M: <code>{}</code> | T: <code>{}</code>" \
-                .format(
-                    format_result(data['madrid']['data'][0]['num']),
-                    format_result(data['madrid']['data'][1]['num']),
-                    format_result(data['madrid']['data'][2]['num']),
-                    format_result(data['madrid']['subtotal']['num']),
-                    format_result(data['madrid']['data'][0]['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['madrid']['data'][1]['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['madrid']['data'][2]['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['madrid']['subtotal']['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['berlin']['data'][0]['num']),
-                    format_result(data['berlin']['data'][1]['num']),
-                    format_result(data['berlin']['data'][2]['num']),
-                    format_result(data['berlin']['subtotal']['num']),
-                    format_result(data['berlin']['data'][0]['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['berlin']['data'][1]['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['berlin']['data'][2]['money'], u"\u20AC", "{:1.2f}"),
-                    format_result(data['berlin']['subtotal']['money'], u"\u20AC", "{:1.2f}")
-                    )
+            message_text = format_message_grouped_data(data, default_grouped_data)
             bot.send_html_message(message.chat.id, message_text)
 
-    ## /money
-    @bot.message_handler(commands=['money'])
-    def response_orders(message):
-        if not check_auth(message):
-            response_no_access(bot, message);
-            return
-        else:
-            try:
-                money_pct = float(get_money(db_pct))
-            except ValueError:
-                money_pct = '-'
-
-            try:
-                money_krd = float(get_money(db_krd))
-            except ValueError:
-                money_krd = '-'
-
-            total = get_total(money_pct, money_krd)
-            message_text = "<b>MADRID:</b> <code>{}</code>\n<b>BERLIN:</b> <code>{}</code>\n<b>TOTAL:</b> <code>{}</code>" \
-                .format(format_result(money_pct, u"\u20AC", "{:1.2f}"), format_result(money_krd, u"\u20AC", "{:1.2f}"), format_result(total, u"\u20AC", "{:1.2f}"))
-            bot.send_html_message(message.chat.id, message_text)
-
-    ## /wrong_orders
-    @bot.message_handler(commands=['wrong_orders'])
-    def response_wrong_orders(message):
-        if not check_auth(message):
-            response_no_access(bot, message);
-            return
-        else:
-            try:
-                wrong_orders_pct = int(get_num_wrong_orders(db_pct))
-            except ValueError:
-                wrong_orders_pct = '-'
-
-            try:
-                wrong_orders_krd = int(get_num_wrong_orders(db_krd))
-            except ValueError:
-                wrong_orders_krd = '-'
-
-            total = get_total(wrong_orders_pct, wrong_orders_krd)
-            message_text = "<b>MADRID:</b> <code>{}</code>\n<b>BERLIN:</b> <code>{}</code>\n<b>TOTAL:</b> <code>{}</code>" \
-                .format(format_result(wrong_orders_pct), format_result(wrong_orders_krd), format_result(total))
-            bot.send_html_message(message.chat.id, message_text)
-
-    ## /bought_items
-    @bot.message_handler(commands=['bought_items'])
+    ## /purchases
+    @bot.message_handler(commands=['purchases', 'p'])
     def response_bought_items(message):
         if not check_auth(message):
             response_no_access(bot, message);
             return
         else:
-            try:
-                bought_items_pct = int(get_bought_items(db_pct))
-            except ValueError:
-                bought_items_pct = '-'
-
-            try:
-                bought_items_krd = int(get_bought_items(db_krd))
-            except ValueError:
-                bought_items_krd = '-'
-
-            total = get_total(bought_items_pct, bought_items_krd)
-            message_text = "<b>MADRID:</b> <code>{}</code>\n<b>BERLIN:</b> <code>{}</code>\n<b>TOTAL:</b> <code>{}</code>" \
-                .format(format_result(bought_items_pct), format_result(bought_items_krd), format_result(total))
-            bot.send_html_message(message.chat.id, message_text)
-
-    ## /bought_money
-    @bot.message_handler(commands=['bought_money'])
-    def response_bought_items(message):
-        if not check_auth(message):
-            response_no_access(bot, message);
-            return
-        else:
-            try:
-                bought_money_pct = float(get_bought_money(db_pct))
-            except ValueError:
-                bought_money_pct = '-'
-
-            try:
-                bought_money_krd = float(get_bought_money(db_krd))
-            except ValueError:
-                bought_money_krd = '-'
-
-            total = get_total(bought_money_pct, bought_money_krd)
-            message_text = "<b>MADRID:</b> <code>{}</code>\n<b>BERLIN:</b> <code>{}</code>\n<b>TOTAL:</b> <code>{}</code>" \
-                .format(format_result(bought_money_pct, u"\u20AC", "{:1.2f}"), format_result(bought_money_krd, u"\u20AC", "{:1.2f}"), format_result(total, u"\u20AC", "{:1.2f}"))
+            data = manager.get_purchases_data()
+            message_text = format_message_grouped_data(data, default_grouped_data)
             bot.send_html_message(message.chat.id, message_text)
 
     ## default handler for every other text
@@ -170,11 +90,8 @@ def print_help(bot, message):
     commands = {
         'start': 'Get used to the bot',
         'help': 'Gives you information about the available commands',
-        'orders': 'Get the number of correct orders in both hubs',
-        'wrong_orders': 'Get the number of wrong orders in both hubs',
-        'bought_items': 'Get the number of items bought in both hubs',
-        'bought_money': 'Get the fiscal price of the items bought in both hubs',
-        'dev_option': 'This indicates that we are are in dev mode. Delete, please'
+        'orders, /o': 'Get data about the correct orders in both hubs',
+        'purchases, /p': 'Get data about items bought in both hubs',
     }
 
     help_text = "The following commands are available: \n"
@@ -183,58 +100,34 @@ def print_help(bot, message):
         help_text += commands[key] + "\n"
     bot.send_message(message.chat.id, help_text)
 
-def get_num_wrong_orders(db):
-    today = time.strftime("%Y-%m-%d") + ' 00:00:00'
-    try:
-        db.query("""
-            SELECT COUNT(*) AS num
-            FROM ps_orders o
-            LEFT JOIN PercentilOrder po
-            ON o.id_order = po.id_order
-            WHERE o.date_add >= '{}'
-            AND (po.id_order IS NULL OR po.id_order_state IN ({}))
-            """.format(today, config.wrong_order_states))
-        r = db.store_result()
-        data = r.fetch_row()
-        return data[0][0]
-    except:
-        return '-'
+def format_message_grouped_data(data, grouped_data):
+    message_text = ''
+    for hub in hubs:
+        message_text += '\n\n<b>{}</b>'.format(hub['name'].upper())
 
-def get_bought_items(db):
-    today = time.strftime("%Y-%m-%d") + ' 00:00:00'
-    try:
-        db.query("""
-            SELECT COUNT(*) AS num
-            FROM PercentilProduct pp
-            WHERE pp.processedOn >= '{}'
-            AND pp.status NOT IN ('Lost', 'Discarded', 'Discarded_Hiper')
-            """.format(today))
-        r = db.store_result()
-        data = r.fetch_row()
-        return data[0][0]
-    except:
-        return '-'
+        for gd in grouped_data:
+            message_text += "\n\t\t" + u"\u2192".format(gd['name'])
+            if data[hub['name']]['data'] is None:
+                message_text += ' -'
+            else:
+                for value in data[hub['name']]['data']:
+                    message_text += ' {}: <code>{}</code>' \
+                    .format(
+                        value['subtype'],
+                        format_result(value[gd['key']], gd['postfix'], gd['format'])
+                    )
 
-def get_bought_money(db):
-    today = time.strftime("%Y-%m-%d") + ' 00:00:00'
-    try:
-        db.query("""
-            SELECT SUM(pp.fiscal_price) AS num
-            FROM PercentilProduct pp
-            WHERE pp.processedOn >= '{}'
-            AND pp.status NOT IN ('Lost', 'Discarded', 'Discarded_Hiper')
-            """.format(today))
-        r = db.store_result()
-        data = r.fetch_row()
-        result = data[0][0]
-        if result is None:
-            return 0
-        else:
-            return result
-    except:
-        return '-'
+        for gd in grouped_data:
+            if data[hub['name']]['data'] is not None:
+                message_text += '\n<b>{}:</b> <code>{}</code>' \
+                    .format(
+                        gd['name'],
+                        format_result(data[hub['name']]['subtotal'][gd['key']], gd['postfix'], gd['format'])
+                    )
+    
+    return message_text
 
-def format_result(value, postfix=None, _format="{:1.0f}"):
+def format_result(value, postfix=None, _format='{:1.0f}'):
     if value == "-":
         return value
     else:
