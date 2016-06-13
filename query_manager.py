16a09688fd1ba388b9d7cd210ebcbf9d77cc7fd2
Rafael Matito <rafa.matito@gmail.com>
2016-06-13 11:05:25 +0200

    Improvements to do interactive mode

--- query_manager.py
+++ query_manager.py
@@ -3,88 +3,87 @@ import config
 
 class Manager():
 
-    def __init__(self, hubs):
-        self.hubs = hubs;
-
-    def get_orders_data(self):
+    def get_orders_data(self, hub, grouping, time_start, time_end):
         result = {}
+        sql = getattr(globals()['Manager'](), 'sql_' + grouping)()
+        data = hub['db'].run_query(sql.format(time_start, time_end, config.valid_order_states))
 
-        for hub in self.hubs:
-            time_start = time.strftime("%Y-%m-%d") + ' 00:00:00'
-            time_end = time.strftime("%Y-%m-%d") + ' 23:59:59'
-            sql = """
-                SELECT po.clothes_subtype AS subtype, 
-                COUNT(*) AS num, sum(total_paid_real) as money
-                FROM ps_orders o
-                INNER JOIN PercentilOrder po
-                ON o.id_order = po.id_order
-                WHERE o.date_add >= '{}'
-                AND o.date_add <= '{}'
-                AND po.id_order_state IN ({})
-                GROUP BY po.clothes_subtype
-                ORDER BY clothes_subtype ASC
-                """.format(time_start, time_end, config.valid_order_states)
+        return self.totalize(data, ['num', 'money'])
+
+    def totalize(self, data, keys):
+        total = {}
+        for key in keys:
+            total[key] = 0
+
+        if data == None:
+            for key in keys:
+                total[key] = '-'
+        else:
+            for value in data:
+                for key in keys:
+                    total[key] += value[key]
+
+        return {'data': data, 'total': total}
+
+    def sql_orders_by_clothes_subtype(self):
+        return """
+            SELECT po.clothes_subtype AS subtype, 
+            COUNT(*) AS num, sum(total_paid_real) as money
+            FROM ps_orders o
+            INNER JOIN PercentilOrder po
+            ON o.id_order = po.id_order
+            WHERE o.date_add >= '{}'
+            AND o.date_add <= '{}'
+            AND po.id_order_state IN ({})
+            GROUP BY po.clothes_subtype
+            ORDER BY clothes_subtype ASC
+            """
+
+    # def get_purchases_data(self):
+    #     result = {}
+
+    #     for hub in self.hubs:
+    #         #time_start = time.strftime("%Y-%m-%d") + ' 00:00:00'
+    #         #time_end = time.strftime("%Y-%m-%d") + ' 23:59:59'
+    #         time_start = '2016-06-03 00:00:00'
+    #         time_end = '2016-06-03 23:59:59'
+    #         sql = """
+    #             SELECT IF(pp.id_subproductType IN (1, 2, 3), 'K', 'W') AS subtype, COUNT(*) AS num, SUM(pp.fiscal_price) AS money
+    #             FROM PercentilProduct pp
+    #             WHERE pp.processedOn >= '{}'
+    #             AND pp.processedOn <= '{}'
+    #             AND pp.status NOT IN ('Lost', 'Discarded', 'Discarded_Hiper')
+    #             GROUP BY subtype
+    #             """.format(time_start, time_end)
         
-            data = hub['db'].run_query(sql)
-            if data == None:
-                subtotal = {'num': '-', 'money': '-'}
-            else:
-                num = money = 0
-                for value in data:
-                    num += value['num']
-                    money += value['money']
+    #         data = hub['db'].run_query(sql)
+    #         if data == None:
+    #             subtotal = {'num': '-', 'money': '-'}
+    #         else:
+    #             num = money = 0
+    #             for value in data:
+    #                 num += value['num']
+    #                 money += value['money']
 
-                subtotal = {'num': num, 'money': money}
+    #             subtotal = {'num': num, 'money': money}
 
-            result[hub['name']] = {'data': data, 'subtotal': subtotal}
+    #         result[hub['name']] = {'data': data, 'subtotal': subtotal}
 
-        return result
+    #     return result
 
-    def get_purchases_data(self):
-        result = {}
-
-        for hub in self.hubs:
-            #time_start = time.strftime("%Y-%m-%d") + ' 00:00:00'
-            #time_end = time.strftime("%Y-%m-%d") + ' 23:59:59'
-            time_start = '2016-06-03 00:00:00'
-            time_end = '2016-06-03 23:59:59'
-            sql = """
-                SELECT IF(pp.id_subproductType IN (1, 2, 3), 'K', 'W') AS subtype, COUNT(*) AS num, SUM(pp.fiscal_price) AS money
-                FROM PercentilProduct pp
-                WHERE pp.processedOn >= '{}'
-                AND pp.processedOn <= '{}'
-                AND pp.status NOT IN ('Lost', 'Discarded', 'Discarded_Hiper')
-                GROUP BY subtype
-                """.format(time_start, time_end)
-        
-            data = hub['db'].run_query(sql)
-            if data == None:
-                subtotal = {'num': '-', 'money': '-'}
-            else:
-                num = money = 0
-                for value in data:
-                    num += value['num']
-                    money += value['money']
-
-                subtotal = {'num': num, 'money': money}
-
-            result[hub['name']] = {'data': data, 'subtotal': subtotal}
-
-        return result
-
-    def get_num_wrong_orders(db):
-        today = time.strftime("%Y-%m-%d") + ' 00:00:00'
-        try:
-            db.query("""
-                SELECT COUNT(*) AS num
-                FROM ps_orders o
-                LEFT JOIN PercentilOrder po
-                ON o.id_order = po.id_order
-                WHERE o.date_add >= '{}'
-                AND (po.id_order IS NULL OR po.id_order_state IN ({}))
-                """.format(today, config.wrong_order_states))
-            r = db.store_result()
-            data = r.fetch_row()
-            return data[0][0]
-        except:
-            return '-'
+    # def get_num_wrong_orders(db):
+    #     today = time.strftime("%Y-%m-%d") + ' 00:00:00'
+    #     try:
+    #         db.query("""
+    #             SELECT COUNT(*) AS num
+    #             FROM ps_orders o
+    #             LEFT JOIN PercentilOrder po
+    #             ON o.id_order = po.id_order
+    #             WHERE o.date_add >= '{}'
+    #             AND (po.id_order IS NULL OR po.id_order_state IN ({}))
+    #             """.format(today, config.wrong_order_states))
+    #         r = db.store_result()
+    #         data = r.fetch_row()
+    #         return data[0][0]
+    #     except:
+    #         return '-'
--- telegram.py
+++ telegram.py
@@ -4,21 +4,27 @@ from daemonize import Daemonize
 import emoji
 import config
 import custombot
+from telebot import types
 import db
+import time
 import query_manager
 
 pid = "/tmp/percentil_telegram.pid"
 
-hubs = [
-    {
-        'name': 'madrid',
-        'db': db.Db(host=config.mad_db_host, user=config.mad_db_user, passwd=config.mad_db_pass, dbname=config.mad_db_name)
-    },
-    {
-        'name': 'berlin',
-        'db': db.Db(host=config.ber_db_host, user=config.ber_db_user, passwd=config.ber_db_pass, dbname=config.ber_db_name)
-    }
-]
+hubs = {
+    'madrid': 
+        {
+            'name': 'Madrid',
+            'db': db.Db(host=config.mad_db_host, user=config.mad_db_user, 
+                passwd=config.mad_db_pass, dbname=config.mad_db_name)
+        },
+    'berlin':
+        {
+            'name': 'Berlin',
+            'db': db.Db(host=config.ber_db_host, user=config.ber_db_user, 
+                passwd=config.ber_db_pass, dbname=config.ber_db_name)
+        }
+}
 
 default_grouped_data = [
     {
@@ -35,12 +41,11 @@ default_grouped_data = [
     }
 ]
 
+userStep = {}
+
 def main():
     bot = custombot.CustomBot(config.token)
-
-
-
-    manager = query_manager.Manager(hubs)
+    manager = query_manager.Manager()
 
     ## /start, /help
     @bot.message_handler(commands=['start', 'help'])
@@ -58,8 +63,11 @@ def main():
             response_no_access(bot, message);
             return
         else:
-            data = manager.get_orders_data()
-            message_text = format_message_grouped_data(data, default_grouped_data)
+            time_start = time.strftime("%Y-%m-%d") + ' 00:00:00'
+            time_end = time.strftime("%Y-%m-%d") + ' 23:59:59'
+
+            data = manager.get_orders_data(hubs['madrid'], 'orders_by_clothes_subtype', time_start, time_end)
+            message_text = format_message_grouped_data(data, hubs['madrid'], default_grouped_data)
             bot.send_html_message(message.chat.id, message_text)
 
     ## /purchases
@@ -72,6 +80,19 @@ def main():
             data = manager.get_purchases_data()
             message_text = format_message_grouped_data(data, default_grouped_data)
             bot.send_html_message(message.chat.id, message_text)
+
+    ## /interactive
+    @bot.message_handler(commands=['interactive'])
+    def command_interactive(message):
+        optionSelect = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        optionSelect.add('one', 'two')
+        bot.send_message(message.chat.id, "Please choose one option", reply_markup=optionSelect)
+        userStep[message.chat.id] = 1
+
+    # if the user has issued the "/interactive" command, process the answer
+    @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
+    def response_interactive(message):
+        bot.send_message(message.chat.id, "Has elegido: " + message.text)
 
     ## default handler for every other text
     @bot.message_handler(func=lambda message: True, content_types=['text'])
@@ -100,30 +121,29 @@ def print_help(bot, message):
         help_text += commands[key] + "\n"
     bot.send_message(message.chat.id, help_text)
 
-def format_message_grouped_data(data, grouped_data):
+def format_message_grouped_data(data, hub, grouped_data):
     message_text = ''
-    for hub in hubs:
-        message_text += '\n\n<b>{}</b>'.format(hub['name'].upper())
+    message_text += '\n\n<b>{}</b>'.format(hub['name'].upper())
 
-        for gd in grouped_data:
-            message_text += "\n\t\t" + u"\u2192".format(gd['name'])
-            if data[hub['name']]['data'] is None:
-                message_text += ' -'
-            else:
-                for value in data[hub['name']]['data']:
-                    message_text += ' {}: <code>{}</code>' \
-                    .format(
-                        value['subtype'],
-                        format_result(value[gd['key']], gd['postfix'], gd['format'])
-                    )
+    for gd in grouped_data:
+        message_text += "\n\t\t" + u"\u2192".format(gd['name'])
+        if data['data'] is None:
+            message_text += ' -'
+        else:
+            for value in data['data']:
+                message_text += ' {}: <code>{}</code>' \
+                .format(
+                    value['subtype'],
+                    format_result(value[gd['key']], gd['postfix'], gd['format'])
+                )
 
-        for gd in grouped_data:
-            if data[hub['name']]['data'] is not None:
-                message_text += '\n<b>{}:</b> <code>{}</code>' \
-                    .format(
-                        gd['name'],
-                        format_result(data[hub['name']]['subtotal'][gd['key']], gd['postfix'], gd['format'])
-                    )
+    for gd in grouped_data:
+        if data['data'] is not None:
+            message_text += '\n<b>{}:</b> <code>{}</code>' \
+                .format(
+                    gd['name'],
+                    format_result(data['total'][gd['key']], gd['postfix'], gd['format'])
+                )
     
     return message_text
 
@@ -136,6 +156,15 @@ def format_result(value, postfix=None, _format='{:1.0f}'):
             return _str + postfix
         else:
             return _str
+
+# error handling if user isn't known yet
+# (obsolete once known users are saved to file, because all users
+#   had to use the /start command and are therefore known to the bot)
+def get_user_step(uid):
+    if uid in userStep:
+        return userStep[uid]
+    else:
+        return 0
 
 main()
 
