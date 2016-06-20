ed79befc3e54afab491ea8879872f208b5c93125
Rafael Matito <rafa.matito@gmail.com>
2016-06-20 15:29:11 +0200

    Open and close connections instead of reusing the existing

--- db.py
+++ db.py
@@ -4,14 +4,27 @@ import MySQLdb.cursors
 class Db():
 
     def __init__(self, host, user, passwd, dbname, port=3306):
-        self._db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=dbname, cursorclass=MySQLdb.cursors.DictCursor)
+        self.host = host
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+        self.dbname = dbname
+
+    def connect(self):
+        self._db = MySQLdb.connect(host=self.host, port=self.port, user=self.user, 
+            passwd=self.passwd, db=self.dbname, cursorclass=MySQLdb.cursors.DictCursor)
+
+    def close():
+        self._db.MySQLdb.close()
 
     def run_query(self, sql):
         try:
+            self.connect()
             c = self._db.cursor()
             c.execute(sql)
             data = c.fetchall()
+            self.close()
             return data
         except Exception as e:
-            print(e)
+            self.close()
             return None
